d93c98c35a207860412c63973b74fd602475f153
Noah Flint <noah.flint24@gmail.com>
2019-07-19 10:26:34 -0600

    created members method

--- app/facades/westeros_facade.rb
+++ app/facades/westeros_facade.rb
@@ -5,13 +5,18 @@ class WesterosFacade
   end
 
   def member_count
+    members.count
+  end
+
+  def members
     conn = Faraday.new('http://westerosapi.herokuapp.com/api/v1/') do |f|
       f.params['api_key'] = ENV['WESTEROS_API_KEY']
       f.adapter Faraday.default_adapter
     end
     response = conn.get("house/#{@house}")
     members = JSON.parse(response.body)['data'][0]['attributes']['members']
-    members.count
+    members.map do |member|
+      Member.new(member)
+    end
   end
-
 end
