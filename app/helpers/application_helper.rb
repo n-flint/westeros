ed34da864398a9f9c8346bf7f85503ae02113f30
Noah Flint <noah.flint24@gmail.com>
2019-07-19 09:39:31 -0600

    added houses method to application helper for dropdown menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def houses
+    [
+      ['Stark', 'stark'],
+      ['Lannister', 'lannister'],
+      ['Targaryen', 'targaryen'],
+      ['Tyrell', 'tyrell'],
+      ['Greyjoy', 'greyjoy']
+    ]
+  end
+
 end
